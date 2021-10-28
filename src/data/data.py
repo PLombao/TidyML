18295a5d3e9cf7c24653c3f57fe7f57d7815dae0
PLombao <pablombao@gmail.com>
2021-10-28 13:44:27 +0000

    update data

--- src/data/data.py
+++ src/data/data.py
@@ -4,6 +4,15 @@ from split_data import split_data
 
 class Data():
     """
+    Abstract class representing the origin data to perform a ML train.
+
+    Methods:
+        import_data:    import the data from the source/s
+        filter_data:    select the instances of the data to train and pre-clean it (clean inference-input wise)
+        split_data:     perform the split of the data in [train, test, validation, others]
+
+    Attrs:
+        
     """
 
     def __init__(self):
