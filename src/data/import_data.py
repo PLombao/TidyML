dd86732e6ef8aec9a3a9fe7948ac15f7ad260a1a
Pablo Lombao <38129612+PLombao@users.noreply.github.com>
2021-11-04 17:30:36 +0100

    Update import_data.py

--- src/data/import_data.py
+++ src/data/import_data.py
@@ -1,4 +1,37 @@
 import pandas as pd
+from google.cloud import bigquery
+
+def get_bq_table(table, date_partition_field, date_field, first_day, last_day, column=None):
+    """Descarga una tabla de BQ en las fechas dadas.
+    
+    Args:
+        table (str):                  URl de tabla BQ (ej: 'mllifecycledxc.landing_prodiq.prodiq_preparation')
+        date_partition_field (str):   Campo de la tabla BQ que se utiliza para particionar
+        date_field (str):             Campo de la tabla BQ que almacena la fecha
+        first_day (str):              Primer día a descargar en formato YYYY-MM-DD
+        last_dary (str):              Último día a descargar en formato YYYY-MM-DD
+        column (str):                 Nombre de columna o columnas separadas por comas (por defecto, todas)
+    
+    Return:
+        pd.DataFrame()
+    """
+    
+    first_month_date = first_day.split("-")[0] + "-" + first_day.split("-")[1] + "-01"
+    last_month_date = last_day.split("-")[0] + "-" + last_day.split("-")[1] + "-01"
+    if column is None:
+        select = "*"
+    else:
+        select = column
+    
+    query = f"""
+    SELECT {select} FROM {table}
+    WHERE DATE({date_partition_field}) >= "{first_month_date}" AND DATE({date_partition_field}) <= "{last_month_date}"
+    AND DATE({date_field}) >= "{first_day}" AND DATE({date_field}) <= "{last_day}"
+    ORDER BY {date_field} ASC"""
+    client = bigquery.Client()
+    result = client.query(query).to_dataframe()
+    
+    return result
 
 def reduce_memory_usage(df, verbose=True):
     numerics = ["int8", "int16", "int32", "int64", "float16", "float32", "float64"]
